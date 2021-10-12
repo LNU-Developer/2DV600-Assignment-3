8a32fcae82f2bf4cc0af10dcea0fec4f1d20cfbe
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2021-10-12 21:31:08 +0200

    Adding post order

--- rm222jx/src/rm222jx/MyDFS.java
+++ rm222jx/src/rm222jx/MyDFS.java
@@ -6,6 +6,7 @@ import graphs.Node;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Iterator;
 
@@ -36,11 +37,93 @@ public class MyDFS<E> implements DFS<E> {
         // eachother, as such I need to check heads independantly
 
         Set<Node<E>> visitedNodes = new LinkedHashSet<>();
-
         Iterator<Node<E>> heads = graph.heads();
         heads.forEachRemaining(node -> performDFS(node, visitedNodes));
 
         return new ArrayList<Node<E>>(visitedNodes);
+    }
+
+    /**
+     * Returns a list of nodes ordered as post-order of the depth first tree
+     * resulting from a depth first search starting at the given root node. Notice,
+     * it only visits nodes reachable from given root node.
+     * </p>
+     * The algorithm also attaches a post-order number to each visited node.
+     */
+    public List<Node<E>> postOrder(DirectedGraph<E> g, Node<E> root) {
+        nullChecker(g);
+        nullChecker(root);
+
+        // RMC: Linked hashsset was picked due to the properties of the HashSet as well
+        // as our need to preserve order. Our goal is to find all visited nodes.
+        Set<Node<E>> visitedNodes = new LinkedHashSet<>();
+        List<Node<E>> postOrderNodes = new LinkedList<>();
+        performPostOrder(root, visitedNodes, postOrderNodes);
+        return postOrderNodes;
+    }
+
+    /**
+     * Returns a list of ALL nodes in the graph ordered as post-order of the depth
+     * first forest resulting from depth first search starting at arbitrary start
+     * nodes.
+     * </p>
+     * The algorithm also attaches a post-order number to each visited node.
+     */
+    public List<Node<E>> postOrder(DirectedGraph<E> g) {
+        nullChecker(g);
+        Set<Node<E>> visitedNodes = new LinkedHashSet<>();
+        List<Node<E>> postOrderNodes = new LinkedList<>();
+        // RMC: In this case I perform iteration on the whole graph
+        Iterator<Node<E>> nodes = g.iterator();
+        nodes.forEachRemaining(node -> performPostOrder(node, visitedNodes, postOrderNodes));
+        return postOrderNodes;
+    }
+
+    /**
+     * Returns a list of ALL nodes in the graph ordered as post-order of the depth
+     * first forest resulting from depth first search starting at arbitrary start
+     * nodes.
+     * </p>
+     * The algorithm attaches a depth-first number if <tt>attach_dfs_number</tt> is
+     * <tt>true</tt>, otherwise it attaches a post-order number.
+     */
+    public List<Node<E>> postOrder(DirectedGraph<E> g, boolean attach_dfs_number) {
+        nullChecker(g);
+        Set<Node<E>> visitedNodes = new LinkedHashSet<>();
+        List<Node<E>> postOrderNodes = new LinkedList<>();
+
+        // RMC: In this case I perform iteration on all heads, attaching a DFS number to
+        // them.
+        Iterator<Node<E>> heads = g.heads();
+        heads.forEachRemaining(node -> {
+            if (attach_dfs_number) {
+                // RMC: Adding all numbering to the current node. Since this increases with the
+                // visited set size I can remove the need for keeping a global property.
+                node.num = visitedNodes.size();
+            }
+
+            performPostOrder(node, visitedNodes, postOrderNodes);
+        });
+        return postOrderNodes;
+    }
+
+    /**
+     * Returns <tt>true</tt> if the graph contains one or more cycles, otherwise
+     * <tt>false</tt>
+     */
+    public boolean isCyclic(DirectedGraph<E> graph) {
+        return false;
+    }
+
+    /**
+     * Returns a list of all nodes in the graph ordered topological. The algorithm
+     * assumes that the graph is acyclic. The result for graphs with cycles are
+     * undefined.
+     */
+    public List<Node<E>> topSort(DirectedGraph<E> graph) {
+        nullChecker(graph);
+        List<Node<E>> x = new ArrayList<Node<E>>();
+        return x;
     }
 
     private void performDFS(Node<E> currentNode, Set<Node<E>> visitedNodes) {
@@ -62,64 +145,27 @@ public class MyDFS<E> implements DFS<E> {
         successors.forEachRemaining(successor -> performDFS(successor, visitedNodes));
     }
 
-    /**
-     * Returns a list of nodes ordered as post-order of the depth first tree
-     * resulting from a depth first search starting at the given root node. Notice,
-     * it only visits nodes reachable from given root node.
-     * </p>
-     * The algorithm also attaches a post-order number to each visited node.
-     */
-    public List<Node<E>> postOrder(DirectedGraph<E> g, Node<E> root) {
-        nullChecker(g);
-        nullChecker(root);
-        List<Node<E>> x = new ArrayList<Node<E>>();
-        return x;
-    }
+    private void performPostOrder(Node<E> currentNode, Set<Node<E>> visitedNodes, List<Node<E>> postOrderedNodes) {
+        // RMC: If it already visited this node, it shoulnd't proceed
+        if (visitedNodes.contains(currentNode))
+            return;
 
-    /**
-     * Returns a list of ALL nodes in the graph ordered as post-order of the depth
-     * first forest resulting from depth first search starting at arbitrary start
-     * nodes.
-     * </p>
-     * The algorithm also attaches a post-order number to each visited node.
-     */
-    public List<Node<E>> postOrder(DirectedGraph<E> g) {
-        nullChecker(g);
-        List<Node<E>> x = new ArrayList<Node<E>>();
-        return x;
-    }
+        // RMC: Adding current node to visited list reference (this will be saved for
+        // the future due to it being a reference)
+        visitedNodes.add(currentNode);
 
-    /**
-     * Returns a list of ALL nodes in the graph ordered as post-order of the depth
-     * first forest resulting from depth first search starting at arbitrary start
-     * nodes.
-     * </p>
-     * The algorithm attaches a depth-first number if <tt>attach_dfs_number</tt> is
-     * <tt>true</tt>, otherwise it attaches a post-order number.
-     */
-    public List<Node<E>> postOrder(DirectedGraph<E> g, boolean attach_dfs_number) {
-        nullChecker(g);
-        List<Node<E>> x = new ArrayList<Node<E>>();
-        return x;
-    }
+        // RMC: Finds all successors of a node
+        Iterator<Node<E>> successors = currentNode.succsOf();
 
-    /**
-     * Returns <tt>true</tt> if the graph contains one or more cycles, otherwise
-     * <tt>false</tt>
-     */
-    public boolean isCyclic(DirectedGraph<E> graph) {
-        return false;
-    }
+        // RMC: runs this operation as long as there are nodes
+        successors.forEachRemaining(successor -> performPostOrder(successor, visitedNodes, postOrderedNodes));
 
-    /**
-     * Returns a list of all nodes in the graph ordered topological. The algorithm
-     * assumes that the graph is acyclic. The result for graphs with cycles are
-     * undefined.
-     */
-    public List<Node<E>> topSort(DirectedGraph<E> graph) {
-        nullChecker(graph);
-        List<Node<E>> x = new ArrayList<Node<E>>();
-        return x;
+        // RMC: Adding all numbering to the current node. Since this increases with the
+        // visited set size I can remove the need for keeping a global property.
+        currentNode.num = postOrderedNodes.size();
+
+        // RMC: Finally adding current node to the postOrderedNodes
+        postOrderedNodes.add(currentNode);
     }
 
     private void nullChecker(DirectedGraph<E> g) {
